3651362528a053713ad47199fb6a4846ad9a5989
paperscreen <139633725+paperscreen@users.noreply.github.com>
2026-07-05 13:52:13 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://paperscreen.example',
+  site: 'https://paperscreen.github.io',
   output: 'static',
   markdown: {
     syntaxHighlight: 'shiki'
